a93064728ae1e0b040c8112ae5c9b2593bddfa5d
Zach Garrett <zach@ketari.io>
2024-05-29 17:08:58 -0400

    add log, extract listener

---
+++ src/endpoints/synthesize/index.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import syntheticSample from "../../lib/syntheticSample/index.js";
+import { SyntheticSampleProps } from "../../lib/syntheticSample/src/types.js";
+
+export default async function synthesize(req: Request, res: Response) {
+  const payload: SyntheticSampleProps = req.body;
+
+  // Validate payload here if needed
+
+  // Handle the payload and create a synthetic sample
+
+  const sample = syntheticSample(payload);
+
+  console.log(sample);
+
+  const response = {
+    message: "Synthetic sample created successfully",
+    payload: sample,
+  };
+
+  res.json(response);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import { SyntheticSampleProps } from "./lib/syntheticSample/src/types.js";
-import syntheticSample from "./lib/syntheticSample/index.js";
+import synthesize from "./endpoints/synthesize/index.js";
 
 const app = express();
 
@@ -9,25 +8,10 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post("/synthesize", (req: Request, res: Response) => {
-  const payload: SyntheticSampleProps = req.body;
-
-  // Validate payload here if needed
-
-  // Log the received payload (for demonstration purposes)
-  console.log(payload);
-
-  // Handle the payload and create a synthetic sample
-
-  const sample = syntheticSample(payload);
-
-  const response = {
-    message: "Synthetic sample created successfully",
-    payload: sample,
-  };
-
-  res.json(response);
-});
+app.post(
+  "/synthesize",
+  async (req: Request, res: Response) => await synthesize(req, res)
+);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
